756a9f66aebf0dc7e9f96387d948dc854685b46a
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 13:26:42 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import './styles.css';
+import React, {Component} from 'react';
+import NavBar from './components/NavBar/NavBar.js';
+import Landing from './components/Landing/Landing.js';
+import Jumbotron from './components/Jumbotron/Jumbotron.js';
+import ProjectCardContainer from './components/ProjectCardContainer/ProjectCardContainer.js'
 
-function App() {
-    return (
-        <div className="App">
-            <Navbar />
-            <Hero />
-            <About />
-            <Portfolio />
-            <Skills />
-            <Contact />
-            <Footer />
-        </div>
-    );
+import Skills from './components/Skills/Skills.js'
+import Footer from './components/Footer/Footer.js'
+
+import Background from './assets/images/background-1.jpg';
+
+
+
+class App extends Component{
+
+  render(){
+    return(
+    <>
+      <NavBar/>
+      <Landing
+      title='James Warren'
+      content='Full Stack Developer'
+      image = {Background}
+      />
+      <Jumbotron
+        title='About Me'
+        content="Full-stack web developer with 22 years of Army service. Recently earned a certificate in Full Stack development from Udemy, with newly developed skills in React.js, JavaScript, CSS, and responsive web development. Excited to use my skills and programming techniques to best serve users."
+       />
+      <ProjectCardContainer />
+
+      <Skills />
+
+      <Footer />
+    </>
+    )
+  }
 }
+
 
 export default App;
