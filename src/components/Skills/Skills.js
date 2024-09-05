a484fd896894453c68fe0e0732cedab5f47472dd
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 13:19:59 -0400

    Create Skills.js

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Skills.css';
+
+const skills = (props) => {
+  return (
+    <section className = 'skills'>
+      <div className = 'inner'>
+        <h2>Skills</h2>
+      </div>
+      <div className = 'container'>
+        <div className = 'skillCard' style={{backgroundColor: "rgba(0, 0, 0, 0.07)"}}>
+          <h4>Web Development</h4>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Bootstrap4</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>React.js</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>SQL</li>
+            <li>MongoDB</li>
+            <li>Git/GitHub</li>
+          </ul>
+        </div>
+        <div className = 'skillCard' style={{backgroundColor: "rgba(0, 0, 0, 0.14)"}}>
+          <h4>Workspace Skills</h4>
+          <ul>
+            <li>Fluent in English</li>
+            <li>Exceptional Communicator</li>
+            <li>Windows and Mac OS</li>
+            <li>Critical Thinker</li>
+            <li>Problem Solver</li>
+            <li>Team Player</li>
+            <li>Time Management</li>
+            <li>Adaptability</li>
+            <li>Organization</li>
+            <li>Attention to Detail</li>
+            <li>Leadership</li>
+            <li>Self-Motivated</li>
+            <li>Accountability</li>
+            <li>Quick Learner</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+export default skills;
