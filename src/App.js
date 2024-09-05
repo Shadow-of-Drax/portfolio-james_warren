e0769aad1bf7f6ac97b69d50891d41d0efc50767
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 13:51:16 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProjectCardContainer from './components/ProjectCardContainer/ProjectCardC
 import Skills from './components/Skills/Skills.js'
 import Footer from './components/Footer/Footer.js'
 
-import Background from './media/background-1.jpg';
+import background from './media/background-1.jpg';
 
 
 
@@ -20,7 +20,7 @@ class App extends Component{
       <Landing
       title='James Warren'
       content='Full Stack Developer'
-      image = {Background}
+      image = {background}
       />
       <Jumbotron
         title='About Me'
