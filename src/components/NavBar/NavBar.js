5e6c458e9f012945c7df756dfe5f995bb2a11cc5
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 12:52:51 -0400

    Create NavBar.js

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+const navBar = (props) => {
+  return (
+    <div className = "header">
+      <h1>
+        <a>James Warren</a>
+      </h1>
+      
+    </div>
+  )
+}
+
+export default navBar;
