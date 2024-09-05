ee0f0649cdcbf027256a8421d35f2d3f81fa26bc
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 12:33:51 -0400

    working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import '../styles.css';
+import React from 'react'
+import './Footer.css'
 
-const Footer = () => {
-    return (
-        <footer>
-            <p>&copy; 2024 James Warren. All rights reserved.</p>
-            <p>Follow me on 
-                <a href="www.linkedin.com/in/jameswarren83">LinkedIn</a>, 
-                <a href="https://github.com/Shadow-of-Drax">GitHub</a>, 
-                <a href="https://x.com/James_R_Warren">X</a>
-            </p>
-        </footer>
-    );
-};
+import GitHub from '../../media/github-logo.png';
+import LinkedIn from '../../media/linkedin.png';
+import Email from '../../media/email.png';
+import Resume from '../../media/writing.png';
 
-export default Footer;
+const footer = (props) => {
+  return (
+    <footer className = 'footer'>
+      <div className='box'>
+        <a href="https://github.com/Shadow-of-Drax" target='_blank'>
+          <img src = {GitHub}/>
+        </a>
+      </div>
+      <div className='box'>
+        <a href='https://www.linkedin.com/in/jameswarren83' target='_blank'>
+          <img src = {LinkedIn} />
+        </a>
+      </div>
+      <div className='box'>
+        <a href = "mailto: james.r.warren83@gmail.com">
+          <img src = {Email} />
+        </a>
+      </div>
+      <div className='box'>
+        <a href = '' target='_blank'>
+          <img src = {Resume} />
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default footer;
