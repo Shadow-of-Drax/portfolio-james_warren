09b7c6a290740bd2bede1a16425c5a79701fb52a
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-05 19:56:49 -0400

    Update Picture.js

---
+++ src/components/Picture/Picture.js
@@ -0,0 +1,14 @@
+import react from "react";
+import "./Picture.css";
+
+const picture = (props) => {
+  return (
+    <div className="header">
+      <h1>
+        <a>James Warren</a>
+      </h1>
+    </div>
+  );
+};
+
+export default picture;
