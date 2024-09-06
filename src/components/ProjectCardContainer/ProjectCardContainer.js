58cb9e3695216026f2c0ea0d3bf7d33f3f9da42f
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-06 16:13:51 -0400

    Update ProjectCardContainer.js

--- src/components/ProjectCardContainer/ProjectCardContainer.js
+++ src/components/ProjectCardContainer/ProjectCardContainer.js
@@ -1,48 +1,47 @@
-import React from 'react'
-import './ProjectCardContainer.css'
-import ProjectCardL from '../ProjectCardL/ProjectCardL.js';
-import ProjectCardR from '../ProjectCardR/ProjectCardR.js';
-import GitHubIcon from '../../components/media/github-logo.png';
-import Cannon from '../../components/media/Cannon.jpeg';
-import Battleshippreview from '../../components/media/Battleshippreview.png';
-
+import React from "react";
+import "./ProjectCardContainer.css";
+import ProjectCardL from "../ProjectCardL/ProjectCardL.js";
+import ProjectCardR from "../ProjectCardR/ProjectCardR.js";
+import GitHubIcon from "../../components/media/github-logo.png";
+import Cannon from "../../components/media/Cannon.jpeg";
+import Battleshippreview from "../../components/media/Battleshippreview.png";
+import Ninja from "../../components/media/Ninja.png";
 
 const ProjectCardContainer = (props) => {
-  return(
+  return (
     <div className="cardContainer">
       <ProjectCardL
-        projectLink='https://github.com/Shadow-of-Drax/Portfolio/tree/main/Battleship'
+        projectLink="https://github.com/Shadow-of-Drax/Portfolio/tree/main/Battleship"
         projectImage={Battleshippreview}
-        title='Battleship'
-        techUsed='html'
-        description='Simple Battleship game created using HTML.'
-        githubLink='https://github.com/Shadow-of-Drax/Portfolio/tree/main/Battleship'
+        title="Battleship"
+        techUsed="html"
+        description="Simple Battleship game created using HTML."
+        githubLink="https://github.com/Shadow-of-Drax/Portfolio/tree/main/Battleship"
         gitHubIcon={GitHubIcon}
         bga="bg1"
       />
       <ProjectCardR
-        projectLink='https://github.com/Shadow-of-Drax/Portfolio/tree/main/my-blog'
+        projectLink="https://github.com/Shadow-of-Drax/Portfolio/tree/main/my-blog"
         projectImage={Cannon}
-        title='My Blog'
-        techUsed='React.js, HTML, CSS, JavaScript'
-        description='Blog created using React.js, HTML, CSS, and JavaScript.'
-        githubLink='https://github.com/Shadow-of-Drax/Portfolio/tree/main/my-blog'
+        title="My Blog"
+        techUsed="React.js, HTML, CSS, JavaScript"
+        description="Blog created using React.js, HTML, CSS, and JavaScript."
+        githubLink="https://github.com/Shadow-of-Drax/Portfolio/tree/main/my-blog"
         gitHubIcon={GitHubIcon}
-        bga='bg3'
+        bga="bg3"
       />
       <ProjectCardL
-        projectLink='https://github.com/Shadow-of-Drax/Portfolio/tree/main/mini-netflix'
-        projectImage={Cannon}
-        title='Mini Netflix'
-        techUsed='React.js, HTML, CSS, JavaScript'
-        description='Netflix clone created using React.js, HTML, CSS, and JavaScript.'
-        githubLink='https://github.com/Shadow-of-Drax/Portfolio/tree/main/mini-netflix'
+        projectLink="https://github.com/Shadow-of-Drax/Portfolio/tree/main/mini-netflix"
+        projectImage={Ninja}
+        title="Back in the SaturDAY"
+        techUsed="React.js, HTML, CSS, JavaScript"
+        description="Streaming service framework showcasing Saturday morning cartoons. This is a work in progress and will be updated as I make progress. Utilizing React.js, HTML, CSS, and JavaScript."
+        githubLink="https://github.com/Shadow-of-Drax/Portfolio/tree/main/mini-netflix"
         gitHubIcon={GitHubIcon}
         bga="bg5"
       />
     </div>
-
-  )
-}
+  );
+};
 
 export default ProjectCardContainer;
