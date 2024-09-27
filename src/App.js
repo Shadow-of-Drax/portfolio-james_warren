74763bf92488193c1abfbaa313392111513c264a
James Warren <98415151+Shadow-of-Drax@users.noreply.github.com>
2024-09-27 15:01:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,10 @@ class App extends Component {
         />
         <Jumbotron
           title="About Me"
-          content="Full-stack web developer with 22 years of Army service. Recently earned a certificate in Full Stack development from Udemy, with newly developed skills in React.js, JavaScript, CSS, and responsive web development. Excited to use my skills and programming techniques to best serve users."
+          content="Hello! I'm James Warren, a passionate Full Stack Developer with a strong foundation in both front-end and back-end technologies. My journey began with a successful career in the U.S. Army, where I honed my skills in project management, leadership, and adaptability. These experiences have equipped me with a unique perspective on teamwork and problem-solving, which I now apply to my work in web development.
+                    I have recently completed a comprehensive web development bootcamp, where I gained proficiency in JavaScript, React.js, Node.js, and database management with MongoDB and PostgreSQL. My projects, such as a streaming service and a pet boarding website, showcase my ability to create engaging and functional web applications that meet user needs.
+                    I thrive in collaborative environments and am always eager to learn and implement new technologies. With my background in leadership and training, I excel at mentoring others and contributing to team success. Whether it's developing user-friendly interfaces or managing project workflows, I am committed to delivering high-quality results.
+                    Feel free to check out my work on GitHub and my portfolio website, and don’t hesitate to reach out if you’re interested in collaborating!"
         />
         <ProjectCardContainer />
 
